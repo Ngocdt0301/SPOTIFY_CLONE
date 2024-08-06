a9b042ad7daa8a45a5a25950413cac2e4326ef0e
Do Tien Ngoc <ngocdotien@gmail.com>
2024-08-06 22:54:45 +0700

    inital project

---
+++ spotify_clone_be/src/main/java/com/codecake/spotify_clone_be/SpotifyCloneBeApplication.java
@@ -0,0 +1,13 @@
+package com.codecake.spotify_clone_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotifyCloneBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotifyCloneBeApplication.class, args);
+	}
+
+}
---
+++ spotify_clone_be/src/test/java/com/codecake/spotify_clone_be/SpotifyCloneBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecake.spotify_clone_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotifyCloneBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
